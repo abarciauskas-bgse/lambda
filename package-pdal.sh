ff91626843ca7a3ef2ae41bad5a72865ac4b421e
Aimee Barciauskas <aimee@developmentseed.org>
2020-07-28 09:16:22 -0700

    Revert changes to package-pdal.sh

--- package-pdal.sh
+++ package-pdal.sh
@@ -10,11 +10,10 @@ cp -r /build/usr/lib/* $DEPLOY_DIR/lib
 cp -r /build/usr/lib64/* $DEPLOY_DIR/lib
 cp -r /build/usr/bin $DEPLOY_DIR/bin
 cp /usr/lib64/libjpeg.so.62.0.0 $DEPLOY_DIR/lib/
-cp /usr/lib64/libxml2.so.2.9.1 $DEPLOY_DIR/lib/libxml2.so.2
+cp /usr/lib64/libxml2.so.2.9.1 $DEPLOY_DIR/lib/
 cp /usr/lib64/liblzma.so.5.2.2 $DEPLOY_DIR/lib/
 cp /usr/lib64/libtiff.so.5.2.0 $DEPLOY_DIR/lib/
 cp /usr/lib64/libpng.so.3.49.0 $DEPLOY_DIR/lib/
-cp /usr/lib64/libcurl.so $DEPLOY_DIR/lib/libcurl.so.4
 cp /usr/lib/libsqlite3.so.0.8.6 $DEPLOY_DIR/lib/
 rm -rf $DEPLOY_DIR/lib/*.a
 rm -rf $DEPLOY_DIR/bin/projinfo
